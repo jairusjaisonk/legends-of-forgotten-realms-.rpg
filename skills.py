1a99b3dcef007370df93bbb9f429ad116bee7346
jairusjaisonk <jairusjaison.k@gmail.com>
2026-08-17 23:07:59 +0530

    Add files via upload

---
+++ skills.py
@@ -0,0 +1,93 @@
+Skills = {
+
+    "warrior": {
+        "power_strike": {
+            "name": "Power Strike",
+            "damage": 30,
+            "mp_cost": 10},
+
+        "heavy_slash": {
+            "name": "Heavy Slash",
+            "damage": 50,
+            "mp_cost": 20},
+
+        "slash" : {
+            "name" : "slash",
+            "damage" : 20,
+            "mp_cost" : 10}},
+
+    "mage": {
+        "fireball": {
+            "name": "Fireball",
+            "damage": 40,
+            "mp_cost": 15},
+
+        "ice_blast": {
+            "name": "Ice Blast",
+            "damage": 50,
+            "mp_cost": 20},
+
+        "thunder_ball" : {
+            "name" : "Thunder_ball",
+            "damage" : 50,
+            "mp_cost" : 10},
+
+        "lump_of_earth" : {
+            "name" : "Lump of earth",
+            "damage" : 30,
+            "mp_cost" : 10,
+        }},
+
+    "archer": {
+        "power_shot": {
+            "name": "Power Shot",
+            "damage": 35,
+            "mp_cost": 10},
+
+        "arrow_rain": {
+            "name": "Arrow Rain",
+            "damage": 50,
+            "mp_cost": 20}},
+
+    "assassin": {
+        "quick_strike": {
+            "name": "Quick Strike",
+            "damage": 35,
+            "mp_cost": 10},
+
+        "shadow_attack": {
+            "name": "Shadow Attack",
+            "damage": 55,
+            "mp_cost": 20},
+
+        "silent_scream" : {
+            "name" : "silent scream",
+            "damage" : 60,
+            "mp_cost" : 30},
+
+        "back_stab": {
+            "name": "Back stab",
+            "damage": 55,
+            "mp_cost": 20}},
+
+    "berserker": {
+
+        "rage_strike": {
+            "name": "Rage Strike",
+            "damage": 50,
+            "mp_cost": 10},
+
+        "brutal_smash": {
+            "name": "Brutal Smash",
+            "damage": 75,
+            "mp_cost": 20},
+
+        "berserker_rampage": {
+            "name": "Berserker Rampage",
+            "damage": 100,
+            "mp_cost": 30},
+
+        "blood_frenzy": {
+            "name": "Blood Frenzy",
+            "damage": 120,
+            "mp_cost": 40}}}
